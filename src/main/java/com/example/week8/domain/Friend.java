dc6728d75c36631602f5608af956035bd6497d45
limeKIWI <pop78978@gmail.com>
2022-09-20 18:35:46 +0900

    #8 Member.java 엔티티 수정/ Friend.java 관계설정 수정

--- src/main/java/com/example/week8/domain/Friend.java
+++ src/main/java/com/example/week8/domain/Friend.java
@@ -6,26 +6,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
-public class FriendList {
+public class Friend {
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
     @JoinColumn(name = "member_id", nullable = false)
-    @OneToOne (fetch = FetchType.LAZY)
+    @ManyToOne (fetch = FetchType.LAZY)
     private Member member;          // 기준이 되는 맴버
 
     @JoinColumn(name = "friend_id", nullable = false)
-    @ManyToOne (fetch = FetchType.LAZY)
+    @OneToOne (fetch = FetchType.LAZY)
     private Member friend;          // 기준 맴버의 친구들
 
 //    @JoinColumn
