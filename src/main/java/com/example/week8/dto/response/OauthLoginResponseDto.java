199fe82140a3f5a65b3d9669efe15f7ac52cc10e
limeKIWI <pop78978@gmail.com>
2022-09-22 10:00:46 +0900

    #2 (fe요청사항) 소셜로그인 응답으로 회원의 닉네임과 전화번호를 보내기

---
+++ src/main/java/com/example/week8/dto/response/OauthLoginResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.week8.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class OauthLoginResponseDto {
+    private String phoneNumber;
+    private String email;
+}
