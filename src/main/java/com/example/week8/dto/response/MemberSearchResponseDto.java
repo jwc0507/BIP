8f90a78ff8cd83a7dfa8a390da8966ccb5ba13df
limeKIWI <pop78978@gmail.com>
2022-09-22 17:27:20 +0900

    #36 맴버/ 친구 검색 응답 dto 구성

---
+++ src/main/java/com/example/week8/dto/response/MemberSearchResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.week8.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class MemberSearchResponseDto {
+    private Long id;
+    private String nickname;
+    private String profileImgUrl;
+}
