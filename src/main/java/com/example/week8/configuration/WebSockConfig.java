b24ead021285617f78b669cfa61a5abc17bfaae9
limeKIWI <pop78978@gmail.com>
2022-09-28 11:45:02 +0900

    #2 fix

--- src/main/java/com/example/week8/configuration/WebSockConfig.java
+++ src/main/java/com/example/week8/configuration/WebSockConfig.java
@@ -22,7 +22,7 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*")
+        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*")
                 .withSockJS(); // sock.js를 통하여 낮은 버전의 브라우저에서도 websocket이 동작할수 있게 합니다.
     }
 }
