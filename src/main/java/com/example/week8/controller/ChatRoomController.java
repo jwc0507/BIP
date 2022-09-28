b9a86ac6322f3ccf5645d5800104514f6bd790eb
limeKIWI <pop78978@gmail.com>
2022-09-28 18:57:11 +0900

    #101 채팅방에 자신이 있는지확인 api, 채팅방 나가기 api구현, 및 추가 개선

--- src/main/java/com/example/week8/controller/ChatRoomController.java
+++ src/main/java/com/example/week8/controller/ChatRoomController.java
@@ -11,8 +11,22 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class ChatRoomController {
     private final ChatService chatService;
+
+    // 채팅방에 있는지 확인
+    @RequestMapping (value = "/api/chat/member/{eventId}", method = RequestMethod.GET)
+    public ResponseDto<?> getChatMember (@PathVariable Long eventId, HttpServletRequest request) {
+        return chatService.getChatMember(eventId, request);
+    }
+
+    // 채팅메세지 불러오기
     @RequestMapping (value = "/api/chat/message/{eventId}", method = RequestMethod.GET)
     public ResponseDto<?> getMessageLog (@PathVariable Long eventId, HttpServletRequest request) {
         return chatService.getMessage(eventId, request);
+    }
+
+    // 채팅방 나가기
+    @RequestMapping (value = "/api/chat/member/{eventId}", method = RequestMethod.DELETE)
+    public ResponseDto<?> exitChatRoom (@PathVariable Long eventId, HttpServletRequest request) {
+        return chatService.exitChatRoom(eventId, request);
     }
 }
--- src/main/java/com/example/week8/service/ChatService.java
+++ src/main/java/com/example/week8/service/ChatService.java
@@ -1,12 +1,9 @@
 package com.example.week8.service;
 
 import com.example.week8.domain.Member;
-import com.example.week8.domain.chat.ChatMessage;
-import com.example.week8.domain.chat.ChatMessageDto;
-import com.example.week8.domain.chat.ChatRequestDto;
-import com.example.week8.domain.chat.ChatRoom;
-import com.example.week8.domain.enums.MessageType;
+import com.example.week8.domain.chat.*;
 import com.example.week8.dto.response.ResponseDto;
+import com.example.week8.repository.ChatMemberRepository;
 import com.example.week8.repository.ChatMessageRepository;
 import com.example.week8.repository.ChatRoomRepository;
 import com.example.week8.repository.MemberRepository;
@@ -32,6 +29,84 @@ public class ChatService {
     private final ChatRoomRepository chatRoomRepository;
     private final ChatMessageRepository chatMessageRepository;
     private final MemberRepository memberRepository;
+    private final ChatMemberRepository chatMemberRepository;
+
+    // 이미 채팅방에 있는 멤버인지 확인
+    public ResponseDto<?> getChatMember(Long eventId, HttpServletRequest request) {
+        ResponseDto<?> chkResponse = validateCheck(request);
+        if (!chkResponse.isSuccess())
+            return chkResponse;
+        Member member = memberRepository.findById(((Member) chkResponse.getData()).getId()).orElse(null);
+        assert member != null;  // 동작할일은 없는 코드
+
+        ChatRoom chatRoom = chatRoomRepository.findById(eventId).orElse(null);
+
+        Optional<ChatMember> chatMember = chatMemberRepository.findByMemberAndChatRoom(member, chatRoom);
+        if (chatMember.isPresent())
+            return ResponseDto.fail("이미 존재하는 회원입니다.");
+        return ResponseDto.success("채팅방에 없는 회원입니다.");
+    }
+
+    // 채팅방 입장
+    @Transactional
+    public ResponseDto<?> enterChatRoom(ChatRequestDto message, String token) {
+        // 토큰으로 유저찾기
+        Long id = Long.parseLong(tokenProvider.getMemberIdByToken(token));
+        Member member = memberRepository.findById(id).orElse(null);
+        if (member == null) {
+            log.info("토큰오류");
+            return ResponseDto.fail("토큰 오류");
+        }
+        ChatRoom chatRoom = chatRoomRepository.findById(message.getRoomId()).orElse(null);
+        if (chatRoom == null) {
+            log.info("룸 번호 오류");
+            return ResponseDto.fail("룸 번호 오류");
+        }
+        // 이미 채팅방에 있는 멤버면 막아야함.
+        if (chatMemberRepository.findByMemberAndChatRoom(member, chatRoom).isPresent())
+            return ResponseDto.fail("이미 존재하는 회원입니다.");
+        // 없다면 채팅방 멤버목록에 넣기
+        chatMemberRepository.save(ChatMember.builder()
+                .chatRoom(chatRoom)
+                .member(member)
+                .build());
+
+        ChatMessageDto chatMessageDto = ChatMessageDto.builder()
+                .sender("알림")
+                .message(member.getNickname() + "님이 입장하셨습니다.")
+                .build();
+
+        // 메세지 보내기
+        messageTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), chatMessageDto);
+
+//        // 보낸 메세지 저장
+//        ChatMessage chatMessage = ChatMessage.builder()
+//                .chatRoom(chatRoom)
+//                .member(member)
+//                .message(message.getMessage())
+//                .build();
+//        chatMessageRepository.save(chatMessage);
+
+        return ResponseDto.success("입장 성공");
+    }
+
+    // 채팅방 나가기
+    @Transactional
+    public ResponseDto<?> exitChatRoom(Long eventId, HttpServletRequest request) {
+        ResponseDto<?> chkResponse = validateCheck(request);
+        if (!chkResponse.isSuccess())
+            return chkResponse;
+        Member member = memberRepository.findById(((Member) chkResponse.getData()).getId()).orElse(null);
+        assert member != null;  // 동작할일은 없는 코드
+
+        ChatRoom chatRoom = chatRoomRepository.findById(eventId).orElse(null);
+        Optional<ChatMember> chatMember = chatMemberRepository.findByMemberAndChatRoom(member, chatRoom);
+        if (chatMember.isEmpty())
+            return ResponseDto.fail("채팅방에 없는 회원입니다.");
+        chatMemberRepository.deleteById(chatMember.get().getId());
+
+        return ResponseDto.success("나가기 완료");
+    }
 
     // 메세지 보내기
     @Transactional
@@ -43,9 +118,6 @@ public class ChatService {
             log.info("토큰오류");
             return ResponseDto.fail("토큰 오류");
         }
-        // 현재 채팅방의 멤버인지 확인
-        // 아니라면 여기서 반환시키기
-
 
         ChatRoom chatRoom = chatRoomRepository.findById(message.getRoomId()).orElse(null);
         if (chatRoom == null) {
@@ -53,30 +125,11 @@ public class ChatService {
             return ResponseDto.fail("룸 번호 오류");
         }
 
-        // 메세지 타입확인
-        Enum<MessageType> messageTypeEnum = message.getType();
-        String messageType = messageTypeEnum.toString();
-        ChatMessageDto chatMessageDto;  // 타입별로 넣어줄 메세지내용 dto
-        switch (messageType) {
-            case "ENTER":
-                chatMessageDto = ChatMessageDto.builder()
-                        .sender("알림")
-                        .message(member.getNickname() + "님이 입장하셨습니다.")
-                        .build();
-                break;
-            case "TALK":
-                chatMessageDto = ChatMessageDto.builder()
-                        .sender(member.getNickname())
-                        .message(message.getMessage())
-                        .build();
-                break;
-            default:  // 개발용 (오류 검증을 위한 부분, 배포단계에서는 삭제필요)
-                chatMessageDto = ChatMessageDto.builder()
-                        .sender("알림")
-                        .message("메세지 타입이 잘못되었습니다.")
-                        .build();
-                break;
-        }
+        ChatMessageDto chatMessageDto = ChatMessageDto.builder()
+                .sender(member.getNickname())
+                .message(message.getMessage())
+                .build();
+
         // 메세지 보내기
         messageTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), chatMessageDto);
 
@@ -97,6 +150,8 @@ public class ChatService {
         ResponseDto<?> chkResponse = validateCheck(request);
         if (!chkResponse.isSuccess())
             return chkResponse;
+        Member member = memberRepository.findById(((Member) chkResponse.getData()).getId()).orElse(null);
+        assert member != null;
 
         Optional<ChatRoom> getChatRoom = chatRoomRepository.findById(roomId);
         ChatRoom chatRoom;
@@ -105,13 +160,18 @@ public class ChatService {
         else
             return ResponseDto.fail("채팅방을 찾을 수 없습니다.");
 
+        ChatMember chatMember = chatMemberRepository.findByMemberAndChatRoom(member,chatRoom).orElse(null);
+        if(chatMember == null)
+            return ResponseDto.fail("채팅 멤버를 찾을 수 없습니다.");
 
-        List<ChatMessage> chatMessageList = chatMessageRepository.findAllByChatRoomOrderByCreatedAtDesc(chatRoom);
+  //      LocalDateTime localDateTime = LocalDateTime.of(2022, 9, 28, 18, 0, 0);
+
+        List<ChatMessage> chatMessageList = chatMessageRepository.findAllByChatRoomAndCreatedAtGreaterThanEqualOrderByCreatedAtDesc(chatRoom, chatMember.getCreatedAt());
         List<ChatMessageDto> chatMessageDtos = new ArrayList<>();
         for (ChatMessage chatMessage : chatMessageList) {
-            Member member = chatMessage.getMember();
+            Member getMember = chatMessage.getMember();
             ChatMessageDto chatMsgResponseDto = ChatMessageDto.builder()
-                    .sender(member.getNickname())
+                    .sender(getMember.getNickname())
                     .message(chatMessage.getMessage())
                     .build();
             chatMessageDtos.add(chatMsgResponseDto);
