717ed292f032058a21559c1404eecbbe15d81457
limeKIWI <pop78978@gmail.com>
2022-10-03 15:22:01 +0900

    #2 (fe요청) 채팅메세지 가져오기에서 보낸시간 가져오기

--- src/main/java/com/example/week8/domain/chat/ChatMessageDto.java
+++ src/main/java/com/example/week8/domain/chat/ChatMessageDto.java
@@ -3,10 +3,14 @@ package com.example.week8.domain.chat;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Builder
 public class ChatMessageDto {
     private String sender;
     private String message;
+    private LocalDateTime sendTime;
 }
