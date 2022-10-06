27c55d7220061742c91522dba1f81772a51560d5
NavyHubb <sngjae972@gmail.com>
2022-10-06 22:42:13 +0900

    게시글 신고 기능 초안

--- src/main/java/com/example/week8/domain/Member.java
+++ src/main/java/com/example/week8/domain/Member.java
@@ -70,6 +70,8 @@ public class Member extends Timestamped {
     @Enumerated(EnumType.STRING)
     private Authority userRole;     // 유저 권한 (erd에 추가해야함)
 
+    private int reportCnt;
+
     public void updateNickname(String name) {
         this.nickname = name;
     }
@@ -116,5 +118,16 @@ public class Member extends Timestamped {
             this.point += 100;
             this.firstLogin = false;
         }
+    }
+
+    // 신고 횟수 올리기
+    public int addReportCnt() {
+        this.reportCnt++;
+        return reportCnt;
+    }
+
+    // 신용도 차감
+    public void declineCredit(double credit) {
+        this.credit -= credit;
     }
 }
