35bd0f2c62f47a7a276d05b89964d401a2682c4a
limeKIWI <pop78978@gmail.com>
2022-10-10 17:58:59 +0900

    #2 comment 관련 url변경 (comments)

--- src/main/java/com/example/week8/controller/CommentController.java
+++ src/main/java/com/example/week8/controller/CommentController.java
@@ -18,31 +18,31 @@ public class CommentController {
     private final CommentService commentService;
 
     // 댓글 작성하기
-    @RequestMapping(value = "/api/comment/{postId}", method = RequestMethod.POST)
+    @RequestMapping(value = "/api/comments/{postId}", method = RequestMethod.POST)
     public ResponseDto<?> createComment (@PathVariable Long postId, @RequestBody @Valid CommentRequestDto requestDto, HttpServletRequest request) {
         return commentService.createComment(postId, requestDto, request);
     }
 
     // 댓글 목록조회
-    @RequestMapping(value = "/api/comment", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/comments", method = RequestMethod.GET)
     public ResponseDto<?> getCommentList (@RequestParam("postId") Long postId, @PageableDefault(size = 50) Pageable pageable, HttpServletRequest request) {
         return commentService.getCommentList(postId, pageable, request);
     }
 
     // 댓글 수정하기
-    @RequestMapping(value = "/api/comment/update/{commentId}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/api/comments/update/{commentId}", method = RequestMethod.PUT)
     public ResponseDto<?> updateComment (@PathVariable Long commentId, @RequestBody @Valid CommentRequestDto requestDto, HttpServletRequest request) {
         return commentService.updateComment(commentId, requestDto, request);
     }
 
     // 댓글 삭제하기
-    @RequestMapping(value = "/api/comment/delete/{commentId}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/api/comments/delete/{commentId}", method = RequestMethod.PUT)
     public ResponseDto<?> deleteComment(@PathVariable Long commentId, HttpServletRequest request) {
         return commentService.deleteComment(commentId, request);
     }
 
     // 댓글 신고
-    @PostMapping("/api/comment/report/{commentId}")
+    @PostMapping("/api/comments/report/{commentId}")
     public ResponseDto<?> report(@PathVariable Long commentId,
                                  HttpServletRequest request) {
         return commentService.reportComment(commentId, request);
