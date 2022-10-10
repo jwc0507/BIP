5cec7244094ae14ba60931e19172e32994c83c3d
NavyHubb <sngjae972@gmail.com>
2022-10-10 15:42:25 +0900

    #219 약속 자동 컨펌

--- src/main/java/com/example/week8/repository/EventRepository.java
+++ src/main/java/com/example/week8/repository/EventRepository.java
@@ -2,8 +2,10 @@ package com.example.week8.repository;
 
 import com.example.week8.domain.Event;
 import com.example.week8.domain.Member;
+import com.example.week8.domain.enums.EventStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
@@ -11,4 +13,5 @@ import java.util.OptionalInt;
 public interface EventRepository extends JpaRepository<Event, Long> {
     void deleteByMaster(Member member);
     List<Event> findAllByMaster(Member member);
+    List<Event> findAllByEventStatusAndEventDateTimeLessThanEqual(EventStatus eventStatus, LocalDateTime nowMinusOneDay);
 }
